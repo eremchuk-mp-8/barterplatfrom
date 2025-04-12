2bef65c37c4403de339583af7d5654bb738e8d58
eremchuk-mp <72536783+eremchuk-mp-8@users.noreply.github.com>
2025-04-12 15:03:50 +0500

    Update tests.py

--- testproject/ads/tests.py
+++ testproject/ads/tests.py
@@ -1,36 +1,107 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.contrib.auth.models import User
-from ads.models import Ad
+from django.urls import reverse
+from ads.models import Ad, ExchangeProposal
 import random
 
 class AdModelTest(TestCase):
-    def test_ad_creation(self):
-        self.user = User.objects.create_user(username='user1', password='pass')
-        categories = ['Электроника', 'Книги', 'Одежда', 'Игрушки', 'Спорт', 'Бытовая техника']
-        conditions = ['new', 'used']
-        
-        for i in range(10):
-            cat = random.choice(categories)
-            ad = Ad.objects.create(
-                category=cat,
-                user=self.user,
-                title=cat+str(i),
-                description="Описание"+cat+str(i),
-                condition=random.choice(conditions),
-            )
-            print(f"Создано объявление: {ad.title}")
-'''
-    def test_ads_count(self):
-        self.assertEqual(Ad.objects.count(), 10)
+    def setUp(self):
+        self.client = Client()
+        self.user1 = User.objects.create_user(username='user1', password='pass')
+        self.user2 = User.objects.create_user(username='user2', password='pass')
 
-    def test_first_ad_content(self):
-        # Получение первого объявления
-        ad = Ad.objects.first()
+        self.categories = ['Электроника', 'Книги', 'Одежда', 'Игрушки', 'Спорт', 'Бытовая техника']
+        self.conditions = ['new', 'used']
 
-        # Проверка, что первое объявление существует
-        self.assertIsNotNone(ad, "Первое объявление не найдено")
+        cat = random.choice(self.categories)
+        num = str(random.randint(10, 1000))
+        self.ad1 = Ad.objects.create(
+            category=cat,
+            title=cat+num,
+            description='Описание '+cat+num,
+            condition=random.choice(self.conditions),
+            user=self.user1)
+        self.ad2 = Ad.objects.create(
+            category=cat,
+            title=cat+num,
+            description='Описание '+cat+num,
+            condition=random.choice(self.conditions),
+            user=self.user2)
+    
+    def test_create_ad(self):
+        self.client.login(username='user1', password='pass')
+        cat = random.choice(self.categories)
+        num = str(random.randint(10, 1000))
+        response = self.client.post(reverse('create_ad'), {
+            'category': cat,
+            'title': cat+num,
+            'description': 'Описание '+cat+num,
+            'condition': random.choice(self.conditions)
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Ad.objects.filter(title=cat+num).exists())
 
-        # Проверка, что у объявления есть название и описание
-        self.assertTrue(ad.title, "У объявления нет названия")
-        self.assertTrue(ad.description, "У объявления нет описания")
-'''
+    def test_edit_ad(self):
+        self.client.login(username='user1', password='pass')
+        response = self.client.post(reverse('edit_ad', args=[self.ad1.id]), {
+            'title': 'Изменённый заголовок',
+            'description': self.ad1.description,
+            'category': self.ad1.category,
+            'condition': self.ad1.condition
+        })
+        self.ad1.refresh_from_db()
+        self.assertEqual(self.ad1.title, 'Изменённый заголовок')
+
+    # Запрет на редактирование чужих объявлений
+    def test_edit_ad_forbidden(self):
+        self.client.login(username='user1', password='pass')
+        response = self.client.post(reverse('edit_ad', args=[self.ad2.id]), {
+            'title': 'Теперь это моё объявление',
+            'description': self.ad2.description,
+            'category': self.ad2.category,
+            'condition': self.ad2.condition
+        })
+        self.assertEqual(response.status_code, 403)
+
+    def test_delete_ad(self):
+        self.client.login(username='user1', password='pass')
+        response = self.client.post(reverse('delete_ad', args=[self.ad1.id]))
+        self.assertFalse(Ad.objects.filter(id=self.ad1.id).exists())
+        self.assertRedirects(response, reverse('ad_list'))
+
+    # Запрет на удаление чужих объявлений
+    def test_delete_ad_forbidden(self):
+        self.client.login(username='user1', password='pass')
+        response = self.client.post(reverse('delete_ad', args=[self.ad2.id]))
+        self.assertEqual(response.status_code, 403)
+
+    def test_create_proposal(self):
+        self.client.login(username='user1', password='pass')
+        user1_ad = Ad.objects.create(
+            category='Бытовая техника',
+            title='Стиральная машина',
+            description='Описание',
+            condition='used',
+            user=self.user1)
+        response = self.client.post(reverse('create_proposal', args=[self.ad2.id]), {
+            'ad_sender': user1_ad.id,
+            'comment': 'Обмен?'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(ExchangeProposal.objects.filter(ad_sender=user1_ad.id, ad_receiver=self.ad2).exists())
+
+    # Предложение обмена самому себе
+    def test_self_proposal_forbidden(self):
+        self.client.login(username='user1', password='pass')
+        user1_ad = Ad.objects.create(
+            category='Бытовая техника',
+            title='Холодильник',
+            description='Описание',
+            condition='used',
+            user=self.user1)
+        response = self.client.post(reverse('create_proposal', args=[user1_ad.id]), {
+            'ad_sender': user1_ad.id,
+            'comment': 'Самообмен'
+        })
+        self.assertEqual(response.status_code, 403)
+        self.assertFalse(ExchangeProposal.objects.filter(ad_receiver=user1_ad).exists())
